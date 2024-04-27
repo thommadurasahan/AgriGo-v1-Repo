9badf1349a3bdb6df125f915384334b4d929881b
Thilanka415 <dilshanweerasingha415@gmail.com>
2024-04-27 12:53:03 +0530

    Update enter_number.dart

--- flutter_agrigo/lib/enter_number.dart
+++ flutter_agrigo/lib/enter_number.dart
@@ -1,65 +1,81 @@
 import 'package:flutter/material.dart';
 
 class EnterNumber extends StatelessWidget {
-  const EnterNumber({super.key});
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Number'),
-          backgroundColor: const Color.fromARGB(255, 14, 214, 21),
-        ),
         body: Container(
-          width: 429,
-          padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 78),
+          width: 429, 
+          padding: EdgeInsets.symmetric(horizontal: 35, vertical: 78),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const SizedBox(height: 7),
+              SizedBox(height: 7),
               Text(
                 "Welcome to AgriGO",
-                style: Theme.of(context).textTheme.headlineMedium,
+                style: TextStyle(
+                color: Colors.green.shade800, 
+                fontWeight: FontWeight.bold,
+                fontSize: 24,)
               ),
-              const SizedBox(height: 24),
+              SizedBox(height: 24),
               Container(
                 width: 351,
-                margin: const EdgeInsets.only(left: 4, right: 3),
+                margin: EdgeInsets.only(left: 4, right: 3),
                 child: Text(
                   "කරුණාකර ඔබගේ ජංගම දුරකථන අංකය ඇතුලත් කරන්න",
                   maxLines: 2,
                   overflow: TextOverflow.ellipsis,
                   textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.bodyLarge,
+                  style: Theme.of(context).textTheme.bodyText1,
                 ),
               ),
-              const Spacer(flex: 37),
-              const Align(
+              Spacer(flex: 37),
+              Align(
                 alignment: Alignment.centerLeft,
                 child: Padding(
                   padding: EdgeInsets.only(left: 3),
                   child: Text(
                     "දුරකථන අංකය",
-                    style: TextStyle(fontSize: 16, color: Colors.black),
+                    style: TextStyle(fontSize: 14, color: Colors.black),
                   ),
                 ),
               ),
-              const SizedBox(height: 33),
+              SizedBox(height: 10),
               _buildPhoneNumberInput(context),
-              const Spacer(flex: 62),
+              Spacer(flex: 62),
               ElevatedButton(
                 onPressed: () {
                   // Add your action here
-                },
-                child: const Text("ඊළඟ"),
+                }
+
+
+                child:    
+               Text("ඊළඟ",
+                style: TextStyle(color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                minimumSize: Size(350, 50),
+                shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                ),
+                backgroundColor: Color(0xFF25DA15),
+               ),
               ),
-              const SizedBox(height: 14),
+              SizedBox(height: 14),
               ElevatedButton(
                 onPressed: () {
                   // Add your action here
                 },
-                child: const Text("මඟ හරින්න"),
+                child: Text("මඟ හරින්න"),
+                style: ElevatedButton.styleFrom(
+                minimumSize: Size(350, 50),
+                shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                ),
+                backgroundColor: Colors.white,
+               ),
               ),
             ],
           ),
@@ -70,19 +86,19 @@ class EnterNumber extends StatelessWidget {
 
   Widget _buildPhoneNumberInput(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 3),
+      padding: EdgeInsets.symmetric(horizontal: 3),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Image.asset(
-            "assets/User.png",
+            "assets/Flag.png",
             height: 40,
             width: 40,
             fit: BoxFit.cover,
           ),
-          const SizedBox(width: 10),
-          const SizedBox(width: 10),
-          const SizedBox(
+          SizedBox(width: 10),
+          SizedBox(width: 10),
+          Container(
             width: 250,
             child: TextField(
               decoration: InputDecoration(
