e011343ac1775edecb1777e390b5bfae362fb5b5
Sahan Praneeth <thommadurasahan@outlook.com>
2024-04-28 11:51:00 +0530

    checknumber.dart renamed
    checknumber.dart renamed to number_check_popup_sin.dart

--- flutter_agrigo/lib/login_page.dart
+++ flutter_agrigo/lib/login_page.dart
@@ -1,58 +1,77 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatelessWidget 
+{
   const LoginPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context) 
+  {
+    return MaterialApp
+    (
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
+      home: Scaffold
+      (
+        appBar: AppBar
+        (
           title: const Text('ලොග් වීම'),
           backgroundColor: const Color.fromARGB(255, 14, 214, 21),
         ),
-        body: Center(
-          child: Column(
+        body: Center
+        (
+          child: Column
+          (
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-
-              const CircleAvatar(
+            children: <Widget>
+            [
+              const CircleAvatar
+              (
               radius: 80.0, // Adjust radius for desired size
               backgroundImage: AssetImage('assets/User.png'), // Replace with your image path
-            ),
-            const SizedBox(height: 20.0), // Add spacing between image and text
-            
-            // User text (replace with actual user data)
-            const Text(
+              ),
+              const SizedBox(height: 20.0), // Add spacing between image and text
+              
+              // User text (replace with actual user data)
+              
+              const Text
+              (
               'ආයුබෝවන්', // Replace with user name variable
               style: TextStyle(fontSize: 18.0),
-            ),
+              ),
 
-              const Padding(
+              const Padding
+              (
                 padding: EdgeInsets.all(16.0),
-                child: TextField(
-                  decoration: InputDecoration(
+                child: TextField
+                (
+                  decoration: InputDecoration
+                  (
                     labelText: 'විද්‍යුත් ලිපිනය',
                   ),
                 ),
               ),
-              const Padding(
+              const Padding
+              (
                 padding: EdgeInsets.all(16.0),
-                child: TextField(
+                child: TextField
+                (
                   obscureText: true,
-                  decoration: InputDecoration(
+                  decoration: InputDecoration
+                  (
                     labelText: 'මුරපදය',
                   ),
                 ),
               ),
-              Container(
+              Container
+              (
                 width: 180,
                 height: 55,
                 padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(
+                child: ElevatedButton
+                (
                 onPressed: () {},
-                style: ElevatedButton.styleFrom(
+                style: ElevatedButton.styleFrom
+                (
                 backgroundColor: const Color.fromARGB(255, 3, 252, 11),
                     ),
                 child: const Text('ලොග් වන්න'),
---
+++ flutter_agrigo/lib/number_check_popup_sin.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+
+class CheckNumber extends StatelessWidget 
+{
+  const CheckNumber({super.key});
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Scaffold
+    (
+      body: SizedBox
+      (
+        width: double.maxFinite,
+        child: Column
+        (
+          children: 
+          [
+            const SizedBox(height: 250),
+            Expanded
+            (
+              child: SingleChildScrollView
+              (
+                child: _buildFour(context),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFour(BuildContext context) 
+  {
+    return Container
+    (
+      margin: const EdgeInsets.only
+      (
+        left: 11,
+        right: 14,
+        bottom: 249,
+      ),
+      padding: const EdgeInsets.symmetric
+      (
+        horizontal: 17,
+        vertical: 31,
+      ),
+      decoration: BoxDecoration
+      (
+        borderRadius: BorderRadius.circular(6),
+        color: const Color(0xFFDDDDDD), // Change color as needed
+      ),
+      child: Column
+      (
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: 
+        [
+          const Padding
+          (
+            padding: EdgeInsets.only(left: 68),
+            child: Text
+            (
+              "අංකය පරික්ශා කිරීම",
+              style: TextStyle
+              (
+                fontSize: 20,
+                fontWeight: FontWeight.bold, // Apply bold font weight
+                ),
+            ),
+          ),
+          
+          const SizedBox(height: 50),
+          Padding
+          (
+            padding: const EdgeInsets.only(left: 0),
+            child: TextField
+            (
+              textAlign: TextAlign.center, // Align text to the center
+              decoration: const InputDecoration
+              (
+                border: OutlineInputBorder(), // Add a border around the TextField
+              ),
+              onTap: () 
+              {
+                // Navigate to the desired screen when tapped
+                //// Navigator.push(context, MaterialPageRoute(builder: (context) => YourNextScreen()));
+              },
+              readOnly: true, // Make the TextField read-only
+              controller: TextEditingController
+              (
+                text: "(+94)-"" -"" -"" -"" -"" -"" -"" -"" " // Set initial text
+              ),
+              style: const TextStyle(fontSize: 25), // Change style as needed
+            ),
+          ),
+
+          const SizedBox(height: 30),
+          const Align
+          (
+            alignment: Alignment.center,
+            child: Text
+            (
+              "ඔබේ දුරකතන අංකය නිවැරැදි ද?",
+              style: TextStyle(fontSize: 16), // Change style as needed
+            ),
+          ),
+          
+          const SizedBox(height: 21),
+          const SizedBox(height: 21),
+          ElevatedButton
+          (
+            onPressed: () {}, // Add onPressed logic as needed
+            style: ElevatedButton.styleFrom
+            (
+              backgroundColor: Colors.green, // Make the button transparent to show container's background color
+            ),
+            child: Container
+            (
+              alignment: Alignment.center,
+              child: const Text
+              (
+                "නිවැරැදියි",
+                style: TextStyle(color: Colors.white), // Set text color to white
+              ),
+            ),
+          ),
+         
+          const SizedBox(height: 27),
+          GestureDetector
+          (
+            onTap: () {
+              // Add your click logic here
+            },
+            child: const Align
+            (
+              alignment: Alignment.center,
+              child: Text
+              (
+                "සංස්කරණය කරන්න",
+                style: TextStyle
+                (
+                  fontSize: 20,
+                  decoration: TextDecoration.underline, // Add underline decoration to indicate clickability
+                ),
+              ),
+            ),
+          ),
+          
+          const SizedBox(height: 37),
+        ],
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: CheckNumber(),
+  ));
+}
