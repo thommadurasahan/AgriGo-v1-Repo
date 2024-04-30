e7fefb40dff526a4c8e5be436a366078e4e3c28e
Sahan Praneeth <thommadurasahan@outlook.com>
2024-04-30 12:09:49 +0530

    Update manu_screen.dart

--- flutter_agrigo/lib/menu_screen.dart
+++ flutter_agrigo/lib/menu_screen.dart
@@ -1,51 +1,52 @@
 import 'package:flutter/material.dart';
 
 class MenuPage extends StatelessWidget {
+  const MenuPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold( 
+      child: Scaffold(
         body: Container(
           width: double.infinity,
-          padding: EdgeInsets.symmetric(horizontal: 2, vertical: 20),
+          padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                "Menu",
-                style: TextStyle(
-                color: Colors.green.shade800, 
-                fontWeight: FontWeight.bold,
-                fontSize: 24,)
-              ),
-              SizedBox(height: 24),
+              Text("Menu",
+                  style: TextStyle(
+                    color: Colors.green.shade800,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  )),
+              const SizedBox(height: 24),
               Expanded(
                 child: ListView(
                   children: [
                     _buildMenuItem(context, "Your Profile"),
-                    Divider( 
-                    thickness: 1, 
-                    color: Colors.black, 
+                    const Divider(
+                      thickness: 1,
+                      color: Colors.black,
                     ),
                     _buildMenuItem(context, "Library "),
-                    Divider( 
-                    thickness: 1, 
-                    color: Colors.black, 
+                    const Divider(
+                      thickness: 1,
+                      color: Colors.black,
                     ),
                     _buildMenuItem(context, "Contact Us"),
-                    Divider( 
-                    thickness: 1, 
-                    color: Colors.black, 
+                    const Divider(
+                      thickness: 1,
+                      color: Colors.black,
                     ),
                     _buildMenuItem(context, "About Us"),
-                    Divider( 
-                    thickness: 1, 
-                    color: Colors.black, 
+                    const Divider(
+                      thickness: 1,
+                      color: Colors.black,
                     ),
                     _buildMenuItem(context, "Terms of Use"),
-                    Divider( 
-                    thickness: 1, 
-                    color: Colors.black, 
+                    const Divider(
+                      thickness: 1,
+                      color: Colors.black,
                     ),
                     _buildMenuItem(context, "Privacy Policy"),
                   ],
@@ -62,8 +63,8 @@ class MenuPage extends StatelessWidget {
     return ListTile(
       title: Row(
         children: [
-          Icon(Icons.arrow_forward),
-          SizedBox(width: 10),
+          const Icon(Icons.arrow_forward),
+          const SizedBox(width: 10),
           Text(title),
         ],
       ),
