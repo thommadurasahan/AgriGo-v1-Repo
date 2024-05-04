611d54508e17e7db74d18a65e871a58a476201d1
HasikaN <159332510+HasikaN@users.noreply.github.com>
2024-05-04 18:14:16 +0530

    createfruitscreen
    createdfruitscreen

---
+++ flutter_agrigo/lib/fruit_screen.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+
+class FruitScreen extends StatefulWidget {
+  @override
+  _FruitScreenState createState() => _FruitScreenState();
+}
+
+class _FruitScreenState extends State<FruitScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AgriGO'),
+        actions: [
+          IconButton(
+            icon: Image.asset('assets/Compass.png'),
+            onPressed: () {
+              // Handle first icon press action
+            },
+          ),
+          IconButton(
+            icon: Image.asset('assets/Switch.png'),
+            onPressed: () {
+              // Handle second icon press action
+            },
+          ),
+        ],
+        backgroundColor: Color(0xFF25DA15),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+             // Add spacing between profile picture and name
+                Text(
+                  'All Fruits', // Replace with your profile name
+                  style: TextStyle(fontSize: 20.0),
+                ),
+            SizedBox(width: 1.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start, // Align profile on right
+              children: [
+                // Replace CircleAvatar with Image
+                Image.asset(
+                  'assets/Fr1.png',
+                  width: 120.0, // Adjust width as needed
+                  height: 120.0, // Adjust height as needed
+                ),
+                SizedBox(width: 5.0), // Add spacing between profile picture and name
+                Text(
+                  'ඇඹුල් කෙසෙල්\nරු 280.00/1kg\nNimal Fruits ', // Replace with your profile name
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                SizedBox(width: 20.0),
+                Image.asset(
+                  'assets/Compass.png',
+                  width: 120.0, // Adjust width as needed
+                  height: 120.0, // Adjust height as needed
+                ),
+              ],
+            ),
+            Divider(
+              thickness: 1.0, // Adjust thickness as needed
+              color: Colors.grey, // Adjust color as needed
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start, // Align profile on right
+              children: [
+                // Replace CircleAvatar with Image
+                Image.asset(
+                  'assets/Fr2.png',
+                  width: 120.0, // Adjust width as needed
+                  height: 120.0, // Adjust height as needed
+                ),
+                SizedBox(width: 5.0), // Add spacing between profile picture and name
+                Text(
+                  'කොමඩු\nරු 1460.00/1kg\nFresh Fruits', // Replace with your profile name
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                SizedBox(width: 20.0),
+                Image.asset(
+                  'assets/Compass.png',
+                  width: 120.0, // Adjust width as needed
+                  height: 120.0, // Adjust height as needed
+                ),
+              ],
+            ),
+            Divider(
+              thickness: 1.0, // Adjust thickness as needed
+              color: Colors.grey, // Adjust color as needed
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start, // Align profile on right
+              children: [
+                // Replace CircleAvatar with Image
+                Image.asset(
+                  'assets/Fr3.png',
+                  width: 120.0, // Adjust width as needed
+                  height: 120.0, // Adjust height as needed
+                ),
+                SizedBox(width: 5.0), // Add spacing between profile picture and name
+                Text(
+                  'අලිගැටපේර\nරු 660.00/1kg\nNew Fruit Lanka', // Replace with your profile name
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                SizedBox(width: 20.0),
+                Image.asset(
+                  'assets/Compass.png',
+                  width: 120.0, // Adjust width as needed
+                  height: 120.0, // Adjust height as needed
+                ),
+              ],
+            ),
+            Divider(
+              thickness: 1.0, // Adjust thickness as needed
+              color: Colors.grey, // Adjust color as needed
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start, // Align profile on right
+              children: [
+                // Replace CircleAvatar with Image
+                Image.asset(
+                  'assets/Fr4.png',
+                  width: 120.0, // Adjust width as needed
+                  height: 120.0, // Adjust height as needed
+                ),
+                SizedBox(width: 5.0), // Add spacing between profile picture and name
+                Text(
+                  'අල අඹ\nරු 480.00/1kg\nNavidu Fruits', // Replace with your profile name
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                SizedBox(width: 30.0),
+                Image.asset(
+                  'assets/Compass.png',
+                  width: 120.0, // Adjust width as needed
+                  height: 120.0, // Adjust height as needed
+                ),
+              ],
+            ),
+            Divider(
+              thickness: 1.0, // Adjust thickness as needed
+              color: Colors.grey, // Adjust color as needed
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start, // Align profile on right
+              children: [
+                // Replace CircleAvatar with Image
+                Image.asset(
+                  'assets/Fr5.png',
+                  width: 120.0, // Adjust width as needed
+                  height: 120.0, // Adjust height as needed
+                ),
+                SizedBox(width: 5.0), // Add spacing between profile picture and name
+                Text(
+                  'අන්නාසි\nරු 2460.00/1kg\nFruit Lanka', // Replace with your profile name
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                SizedBox(width: 30.0),
+                Image.asset(
+                  'assets/Compass.png',
+                  width: 120.0, // Adjust width as needed
+                  height: 120.0, // Adjust height as needed
+                ),
+              ],
+            ),
+            Divider(
+              thickness: 1.0, // Adjust thickness as needed
+              color: Colors.grey, // Adjust color as needed
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
