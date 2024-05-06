c2143cec2a4693fe03f215087f96039a29a1cfc3
Sahan Praneeth <thommadurasahan@outlook.com>
2024-05-06 09:23:17 +0530

    Update main.dart

--- flutter_agrigo/lib/main.dart
+++ flutter_agrigo/lib/main.dart
@@ -1,27 +1,5 @@
 import 'package:flutter/material.dart';
-import 'login_sin.dart';
-import 'signup_sin.dart';
-import 'get_started_sin.dart';
-import 'get_started_eng.dart';
-import 'language_selection.dart';
-import 'menu.dart';
-import 'categories.dart';
-import 'page_screen.dart';
-import 'catogayveg.dart';
-import 'loading.dart';
 import 'home_sin.dart';
-import 'notification.dart';
-import 'veg_screen.dart';
-import 'fish_screen.dart';
-import 'fruit_screen.dart';
-import 'imports_screen.dart';
-import 'others_screen.dart';
-import 'signup_eng.dart';
-import 'login_eng.dart';
-import 'notification.dart';
-import 'home_eng.dart';
-import 'profile.dart';
-import 'library.dart';
 
 void main() {
   runApp(const MyApp());
