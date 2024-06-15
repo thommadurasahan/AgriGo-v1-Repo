6d186288fed5adb6e40d9650ed22e9b9f48cf19b
Sahan Praneeth <thommadurasahan@outlook.com>
2024-06-15 09:27:04 +0530

    Remove unnecessary pubspec.yami

--- flutter_agrigo/lib/login_sin.dart
+++ flutter_agrigo/lib/login_sin.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_agrigo/firebase_options.dart';
 import 'package:flutter_agrigo/get_started_sin.dart';
 
