56e21bac6a7878a17c21ee671391b6a05b079483
Sahan Praneeth <thommadurasahan@outlook.com>
2024-06-18 08:58:49 +0530

    Update main.dart

--- flutter_agrigo/lib/main.dart
+++ flutter_agrigo/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      // Set the title of the app
       home: Loading(),
     );
   }
